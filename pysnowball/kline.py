cb1fc7d0312de894e177a3e66cac1265b3004758
Gavin <yjhuestc@aliyun.com>
2020-01-04 20:52:16 +0800

    add kline

---
+++ pysnowball/kline.py
@@ -0,0 +1,14 @@
+import json
+import os
+from pysnowball import cons
+from pysnowball import api_ref
+from pysnowball import utls
+import time
+
+
+def kline(symbol):
+	time_stamp = int(round(time.time() * 1000))
+	params = '&begin'+str(time_stamp)+'&period=day&count=-142&indicator=kline'
+    url = api_ref.kline_url+symbol+params
+    return utls.fetch(url)
+
